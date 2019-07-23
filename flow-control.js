0bc7948df0b464b21efe04e1f8da7808193d94a6
Matt Kleminsky <mkleminsky@yahoo.com>
2019-07-23 23:04:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,9 @@ function basicTeenager(age) {if (age>12 && age<20)
 
 }
 
-function teenager(age) {
+function teenager(age) {if (age>12 && age<20)
+  {return "You are a teenager!"}
+  else {return "You are not a teenager"}
 
 }
 
